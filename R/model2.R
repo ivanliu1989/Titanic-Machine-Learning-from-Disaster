1ca791b1e3579a484a623711883cd0ce59c56eab
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-25 10:58:28 +1000

    model2

--- R/model2.R
+++ R/model2.R
@@ -1,3 +1,4 @@
+# part1
 setwd("C:\\Documents and Settings\\Macro\\Desktop\\Ivandata\\Titanic-Machine-Learning-from-Disaster\\R")
 train <- read.csv('train.csv',header=T,stringsAsFactor=F)
 test <- read.csv('test.csv',header=T,stringsAsFactor=F)
@@ -7,3 +8,26 @@ prop.table(table(train$Survived))
 test$Survived <- rep(0, 418)
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "theyallperish.csv", row.names = FALSE)
+
+# part2
+table(train$Sex)
+prop.table(table(train$Sex, train$Survived))
+prop.table(table(train$Sex, train$Survived),1)
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
+summary(train$Age)
+train$Child <- 0
+train$Child[train$Age < 18] <- 1
+aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
+aggregate(Survived ~ Child + Sex, data=train, FUN=length)
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+
+# part 3
